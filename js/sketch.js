a9203d0ed9ddbf05ca362adfe14513592b5d02c1
FloloGames <60265085+FloloGames@users.noreply.github.com>
2021-10-17 15:42:31 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,8 @@ let CurrentDownload;
 let TargetDownload;
 let CurrentDownloadSpeed;
 
+let seconds;
+
 let output;
 
 let enterButton;
@@ -21,11 +23,30 @@ function draw(){
 function enterButtonPressed(){
   let restDownload = TargetDownload.value()-CurrentDownload.value();
   let speed = CurrentDownloadSpeed.value();
-  restDownload *= 1000;
+  restDownload *= 1000;//mb
   print('Restdownload : ', restDownload)
-  let restTime = restDownload/speed;//In Seconds
-  restTime /= 60;//Now Minutes
-  restTime = round(restTime);
-  print('RestTime : ', restTime);
-  output.html(restTime + ' min.');
+  seconds = restDownload/speed;//In Seconds
+  //restTime /= 60;//Now Minutes
+  //restTime = round(restTime);
+  //print('RestTime : ', restTime);
+  //output.html(restTime + ' min.');
+  
+  let d = 0;
+  let h = 0;
+  let m = 0;
+  while(seconds >= 60){
+    seconds -= 60;
+    m += 1;
+  }
+  while(m >= 60){
+    m -= 60;
+    h += 1;
+  }
+  while(h >= 24){
+    h -= 24;
+    d += 1;
+  }
+
+  output.html('Days: ' + d + '<br>' + 'Hour: ' + h + '<br>' + 'Minute: ' + m + '<br>' + 'Second: ' + round(seconds));
+
 }
